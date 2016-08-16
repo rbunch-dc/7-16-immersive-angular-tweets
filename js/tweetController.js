a4f40ed863764dfcfd4d1cfe137787c19bc25fda
Robert Bunch <rob@digitalcrafts.com>
2016-08-16 11:32:09 -0400

    Tweet controller with AJAX call

--- js/tweetController.js
+++ js/tweetController.js
@@ -2,5 +2,17 @@ tweetApp.controller('tweetController', function($scope, $http){
 	$scope.test = "I am working!";
 	$scope.tweets = tweets;
 
+	$http({
+		method: 'GET',
+		url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=trump'
+	}).then(
+		function successFunction(tweetData){
+			$scope.tweets = tweetData.data.statuses;
+			console.log(tweetData.data.statuses);
+
+		},function failureFunction(tweetData){
+			console.log(tweetData);
+	});	
+
 });
  
