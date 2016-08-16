e682fe915f59ae9a33601e855a998e7e66bd5a44
Robert Bunch <rob@digitalcrafts.com>
2016-08-16 14:52:42 -0400

    Update to use views and route provider

--- js/tweetController.js
+++ js/tweetController.js
@@ -1,10 +1,19 @@
-tweetApp.controller('tweetController', function($scope, $http){
+tweetApp.controller('tweetController', function($scope, $http, $location, $routeParams){
 	$scope.test = "I am working!";
 	$scope.tweets = tweets;
 
+	// console.log($location.hash());
+	console.log($routeParams);
+
+	if($routeParams.searchTerm){
+		var searchTerm = $routeParams.searchTerm;
+	}else{
+		var searchTerm = 'trump';
+	}
+
 	$http({
 		method: 'GET',
-		url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash=trump'
+		url: 'http://digitalcrafts.com/students/twitter/hashtag.php?user=true&hash='+searchTerm
 	}).then(
 		function successFunction(tweetData){
 			$scope.tweets = tweetData.data.statuses;
--- js/tweetModule.js
+++ js/tweetModule.js
@@ -2,17 +2,22 @@
 var tweetApp = angular.module('tweetApp', ['ngRoute']);
 
 
-// tweetApp.config(function($routeProvider){
-// 	//At / load up home.html, with home controller	
-// 	$routeProvider.when('/',{
-// 		templateUrl: 'views/home.html',
-// 		controller: 'homeController'
-// 	});
-// 	//At /trump, load up the same template, but new controller
-// 	$routeProvider.when('/trump',{
-// 		templateUrl: 'views/home.html',
-// 		controller: 'trumpController'
-// 	});
+tweetApp.config(function($routeProvider){
+	//At / load up home.html, with home controller	
+	$routeProvider.when('/',{
+		templateUrl: 'views/home.html',
+		controller: 'tweetController'
+	});
+	//At /trump, load up the same template, but new controller
+	$routeProvider.when('/:searchTerm',{
+		templateUrl: 'views/home.html',
+		controller: 'tweetController'
+	});
+	$routeProvider.when('/:searchTerm/:searchTerm2',{
+		templateUrl: 'views/home.html',
+		controller: 'tweetController'
+	});
+
 // 	//At /hillary, load up the same template, but new controller
 // 	$routeProvider.when('/hillary',{ 
 // 		templateUrl: 'views/home.html',
@@ -25,18 +30,6 @@ var tweetApp = angular.module('tweetApp', ['ngRoute']);
 // 	})
 // 	$routeProvider.otherwise('/'); //Default
 
-// });
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
 
